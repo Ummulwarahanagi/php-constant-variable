756cbd927a6d3f7ff22c2a8c11a8c96342d89a99
Ummulwarahanagi <154959593+Ummulwarahanagi@users.noreply.github.com>
2025-04-12 12:30:54 +0530

    Create task2.php

---
+++ task2.php
@@ -0,0 +1,12 @@
+<?php
+//variables
+$ClgName="IBMR";
+$Regno=12;
+$Branch="CS";
+//constants
+define('PHONE',9876543212);
+define('DEPT','BCA');
+echo "Clg Name:".$ClgName."<br>"."Reg no:".$Regno."<br>"."Branch :".$Branch."<br>";
+echo "Phone:".PHONE."<br>";
+echo "Department:".DEPT;
+?>
